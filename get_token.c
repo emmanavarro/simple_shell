9cba1bbc2a2ead7886a0821d3bafcec94b5bc037
laucavv <laucavv23@gmail.com>
2020-04-07 21:45:39 +0000

    fix betty

--- get_token.c
+++ get_token.c
@@ -24,41 +24,42 @@ char **get_token(char *str)
 {
 	char **tokens = NULL;
 	char *tok = NULL;
-	int count = 0;
+	int count = 0, i;
 	char *buff = NULL;
 
 	buff = malloc(sizeof(char) * _strlen(str) + 1);
-	if (buffer == NULL)
+	if (buff == NULL)
 		return (NULL);
 	_strcpy(buff, str);
 	tok = strtok(str, " \t\n\r");
 	while (tok)
 	{
 		count++;
-        tok = strtok(NULL, " \t\n\r");
+		tok = strtok(NULL, " \t\n\r");
 	}
-    tokens = _calloc((count + 1), sizeof(char *));
-    if (tokens == NULL)
-    {
+	tokens = _calloc((count + 1), sizeof(char *));
+	if (tokens == NULL)
+	{
 		free(buff);
 		return (NULL);
 	}
 	tok = strtok(buff, " \t\n\r");
 	count = 0;
 	while (tok)
-    {
-        tokens[count] = malloc(sizeof(char) * _strlen(tok) + 1);
-        if (tokens[count] == NULL)
-        {
-            free(tokens);
-            free(buff);
-            return (NULL);
-        }
-        _strcpy(tokens[count], tok);
-        count++;
-        tok = strtok(NULL, " \t\n\r");
-    }
-    tokens[count] = NULL;
-    free(buff);
-    return (tokens);
+	{
+		tokens[count] = malloc(sizeof(char) * _strlen(tok) + 1);
+		if (tokens[count] == NULL)
+		{
+			for (i = 0; tokens[count]; i++)
+				free(tokens[count]);
+			free(tokens), free(buff);
+			return (NULL);
+		}
+		_strcpy(tokens[count], tok);
+		count++;
+		tok = strtok(NULL, " \t\n\r");
+	}
+	tokens[count] = NULL;
+	free(buff);
+	return (tokens);
 }
