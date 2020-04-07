51dea3bd8663b1c985bd5d67494609ff74270545
Emma Navarro <elnavarro55@gmail.com>
2020-04-07 21:46:40 +0000

    Second tokenization

--- get_token.c
+++ get_token.c
@@ -7,6 +7,7 @@
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -14,22 +15,50 @@ int _strlen(char *s)
 	return (i);
 }
 /**
+ * get_token - get the command line and tokenize it
+ * @str: command line
  *
+ * Return: tokenized command
  */
 char **get_token(char *str)
 {
 	char **tokens = NULL;
 	char *tok = NULL;
-	int count;
+	int count = 0;
 	char *buff = NULL;
+
 	buff = malloc(sizeof(char) * _strlen(str) + 1);
-	if (buffer != NULL)
+	if (buffer == NULL)
 		return (NULL);
 	_strcpy(buff, str);
 	tok = strtok(str, " \t\n\r");
 	while (tok)
 	{
 		count++;
-		tok = strtok(NULL, " \t\n\r");
+        tok = strtok(NULL, " \t\n\r");
 	}
+    tokens = _calloc((count + 1), sizeof(char *));
+    if (tokens == NULL)
+    {
+		free(buff);
+		return (NULL);
+	}
+	tok = strtok(buff, " \t\n\r");
+	count = 0;
+	while (tok)
+    {
+        tokens[count] = malloc(sizeof(char) * _strlen(tok) + 1);
+        if (tokens[count] == NULL)
+        {
+            free(tokens);
+            free(buff);
+            return (NULL);
+        }
+        _strcpy(tokens[count], tok);
+        count++;
+        tok = strtok(NULL, " \t\n\r");
+    }
+    tokens[count] = NULL;
+    free(buff);
+    return (tokens);
 }
