a34692f18da33f67716325442aea735432a672be
laucavv <laucavv23@gmail.com>
2020-04-07 16:25:20 +0000

    Header

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/wait.h>
+
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *_strcat(char *dest, char *src);
+
+char *prompt(void);
+
+#endif /* _SHELL_H*/
