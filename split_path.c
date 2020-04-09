b3a357686b1abb9bb2cff6555ab68757baa7a671
laucavv <laucavv23@gmail.com>
2020-04-09 02:17:24 +0000

    add funtion find and split

--- split_path.c
+++ split_path.c
@@ -1,38 +1,37 @@
 #include "shell.h"
 
-void split_path(char *str, char **env)
+char *find_path(char **env)
 {
-	printf ("llego");
 	int j = 0;
 	char *cpy_env = NULL, *store_path = NULL;
-
-	cpy_env = malloc(sizeof(char) * _strlen(env[j])+ 1);
-
-	if (cpy_env == NULL)
-	{
-		printf("null");
-	}
-	_strcpy(cpy_env, env[j]);
-
+	cpy_env = env[j];
 	store_path = strtok(cpy_env, "=");
 
 	while (_strncmp("PATH", store_path, 4) && store_path  && env[j])
 	{
-		 _strcpy(cpy_env, env[j]);
+		cpy_env = env[j];
 		store_path = strtok(cpy_env, "=");
 		j++;
 	}
-	printf("copy: %s\n", cpy_env);
-	store_path = strtok(NULL, ":");
+	store_path = strtok(NULL, "\n");
 
-
-
-	/*while(store_path != NULL)
-	{
-		store_path= strtok(NULL, ":");
-		_strcat(store_path, "/");
-		_strcat(store_path, str);
-	}*/
-
-	printf("lo hizo store:%s  \n", store_path);
+	return (store_path);
 }
+char **split(char **enviro)
+{
+	char *cpy_path = find_path(enviro);
+	char **store_dir = NULL;
+	char *tok = NULL;
+	int pos = 0;
+
+	tok = strtok(cpy_path, ":");
+	while (tok != NULL)
+	{
+		_strcpy(store_dir[pos], tok);
+		tok = strtok(NULL, ":");
+		pos++;
+
+	}
+	return (store_dir);
+}
+
