9d3bfa08e1abb38dfedeb86fe8b6039cffac6495
laucavv <laucavv23@gmail.com>
2020-04-09 00:11:53 +0000

    add funtion split

--- shell.h
+++ shell.h
@@ -16,8 +16,10 @@ int _strncmp(char *s1, char *s2, int n);
 char *_memset(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_strcat(char *dest, char *src);
+int _strlen(char *s);
 
-char *prompt(int *exit);
+char *prompt();
 char **get_token(char *str);
+void split_path(char *str, char **env);
 
 #endif /* _SHELL_H*/
