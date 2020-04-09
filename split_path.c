18b2775322603ee7370110e93a5c43809730efac
laucavv <laucavv23@gmail.com>
2020-04-09 22:22:42 +0000

    betty

--- split_path.c
+++ split_path.c
@@ -1,10 +1,18 @@
 #include "shell.h"
 
+/**
+ * find_path - find the environment variable PATH
+ * @env: environment variable
+ * Return: PATH
+ */
+
 char *find_path(char **env)
 {
 	int j = 0;
 	char *cpy_env = NULL, *store_path = NULL;
+
 	cpy_env = env[j];
+
 	store_path = strtok(cpy_env, "=");
 
 	while (_strncmp("PATH", store_path, 4) && store_path  && env[j])
@@ -16,40 +24,47 @@ char *find_path(char **env)
 	store_path = strtok(NULL, "\n");
 	return (store_path);
 }
-char *split(char **enviro, char *str)
+/**
+ * split_check - divide the elements and check the path
+ * @enviro: environment variable
+ * @str: element to search in the path
+ * Return: path
+ */
+char *split_check(char **enviro, char *str)
 {
 	char *cpy_path = NULL, *path = NULL;
-	cpy_path = find_path(enviro);
 	char **store_dir;
 	char *tok = NULL;
 	char *dir = NULL;
 	int pos = 0;
 	struct stat st;
+
+	cpy_path = find_path(enviro);
 	store_dir = _calloc(8, sizeof(char *));
 
 	if (store_dir == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	tok = strtok(cpy_path, ":");
 	while (tok != NULL)
 	{
 		dir = _strdup(tok);
 		store_dir[pos] = dir;
+
+		path = _strcat(store_dir[pos], "/");
+		path = _strcat(store_dir[pos], str);
+
+		if (stat(path, &st) == 0)
+		{
+			while (store_dir && store_dir[pos])
+				free(store_dir[pos--]);
+			free(store_dir);
+			return (path);
+		}
 		tok = strtok(NULL, ":");
 		pos++;
 	}
-	pos = 0;
-	while(store_dir[pos] != NULL)
-	{
-		path = _strcat(store_dir[pos], "/");
-		path = _strcat(store_dir[pos], str);
-		if (stat(path, &st) == 0)
-			return (path);
-		pos++;
-	}
-	while(store_dir && store_dir[pos])
-		free(store_dir[pos--]);
-	free(store_dir);
+
 	return (NULL);
 }
