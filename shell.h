d85a629d8570913fded3ebc22002c1ab021a67a7
Emma Navarro <elnavarro55@gmail.com>
2020-04-11 17:12:29 +0000

    Betty ok, valgrind ok

--- shell.h
+++ shell.h
@@ -12,30 +12,29 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-/*
+/**
 * struct command - struct that contains the basic commands
 * @name: name of the command
 * @path: location of the command
 */
 typedef struct command
 {
-    char *name;
-    char *path;
-}com_s;
+	char *name;
+	char *path;
+} com_s;
 
 char *_strcpy(char *dest, char *src);
-int _strncmp(char *s1, char *s2, int n);
+int _strncmp(const char *s1, const char *s2, size_t n);
 char *_memset(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
-char *_strdup(char *str);
 
 char *prompt();
 char **get_token(char *str);
 char *find_path(char **env);
 char *split_check(char **enviro, char *str);
 char *basic_command(char *str);
-void child_process(char **env, char **argv);
+int child_process(char **env, char **argv);
 
 #endif /* _SHELL_H*/
