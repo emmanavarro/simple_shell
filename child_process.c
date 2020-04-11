618326b340e82cd4172968345e3fa4895e043d4c
Emma Navarro <elnavarro55@gmail.com>
2020-04-11 16:59:17 +0000

    Betty ok, valgrind ok

--- child_process.c
+++ child_process.c
@@ -4,37 +4,25 @@
  * child_process - create a child process to execute the command
  * @env: environment
  * @argv: arguments to verify
+ * Return: 0 success, 127 error
  */
-void child_process(char **env, char **argv)
+int child_process(char **env, char **argv)
 {
 	struct stat st;
 	char *str = NULL;
 	int status = 0;
 	pid_t son;
-	int d = 0;
-	int p;
 
 	if (stat(argv[0], &st) == 0)
 		str = argv[0];
 	else
 	{
 		str = basic_command(argv[0]);
-		printf("encontre la ruta en command %s\n", str);
+
 		if (str == NULL)
 		{
-			printf("estoy en path \n");
 			str = split_check(env, argv[0]);
-			d = 1;
-			printf("cambie mi valor y soy D: %d\n",d);
 		}
-	}
-	p = 0;
-
-	while(argv[p] != NULL)
-	{
-		printf("estoy en child argv: %s\n", argv[p]);
-		p++;
-
 	}
 	if (str)
 	{
@@ -43,18 +31,12 @@ void child_process(char **env, char **argv)
 		{
 			if (execve(str, argv, env) == -1)
 				perror("cannot access, No such file or directory");
+				return (127);
 		}
 		else
 			wait(&status);
 	}
 	else
 		perror("command not found");
-	p = 0;
-	while(argv[p] != NULL)
-	{
-		printf("estoy en child argv: %s\n", argv[p]);
-		p++;
-	}	
-
-	printf("ya no estoy aqui\n");
+	return (0);
 }
