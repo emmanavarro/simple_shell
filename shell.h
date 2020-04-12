b77bbb24ce93623a00d5ea52c6eb810d4b6a828a
laucavv <laucavv23@gmail.com>
2020-04-12 03:16:38 +0000

    modified child process and add _strdup

--- shell.h
+++ shell.h
@@ -29,12 +29,13 @@ char *_memset(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
+char *_strdup(char *str);
 
 char *prompt();
 char **get_token(char *str);
 char *find_path(char **env);
 char *split_check(char **enviro, char *str);
 char *basic_command(char *str);
-int child_process(char **env, char **argv);
+int child_process(char **env, char **argv, char *av, int count);
 
 #endif /* _SHELL_H*/
