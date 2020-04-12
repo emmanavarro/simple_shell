6c84103cf192092a4229ca6c2d1392038faf6679
laucavv <laucavv23@gmail.com>
2020-04-12 22:43:52 +0000

    valgrid OK

--- child_process.c
+++ child_process.c
@@ -33,11 +33,15 @@ int child_process(char **env, char **argv, char *av, int count)
 			{
 				sprintf(err_msg, "%s: %d: %s: not found\n", av, count, argv[0]);
 				write(2, err_msg, _strlen(err_msg));
+				free(str);
 				return (127);
 			}
 		}
 		else
+		{
 			wait(&status);
+			free(str);
+		}
 	}
 	else
 	{
