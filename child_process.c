9374648946173a7c21452fb8cc163df3f54f172a
laucavv <laucavv23@gmail.com>
2020-04-14 16:37:48 +0000

    add conditional

--- child_process.c
+++ child_process.c
@@ -12,14 +12,14 @@ int child_process(char **env, char **argv, char *av, int count)
 {
 	struct stat st;
 	char *str = NULL, err_msg[100];
-	int status = 0, (*f)(char **env);
+	int status = 0, (*f)(char **env), i = 0;
 	pid_t son;
 
 	if (stat(argv[0], &st) == 0)
 		str = argv[0];
 	else
 		if (str == NULL)
-			str = split_check(env, argv[0]);
+			str = split_check(env, argv[0]), i = 1;
 	f = basic_command(argv[0]);
 	if (f != NULL)
 	{
@@ -35,17 +35,17 @@ int child_process(char **env, char **argv, char *av, int count)
 			{
 				sprintf(err_msg, "%s: %d: %s: not found\n", av, count, argv[0]);
 				write(2, err_msg, _strlen(err_msg));
-				free(str);
+				(i == 1) ? free(str) : i;
 			}
 		}
 		else
-			wait(&status), free(str);
+			wait(&status), (i == 1) ? free(str) : i;
 	}
 	else
 	{
 		sprintf(err_msg, "%s: %d: %s: not found\n", av, count, argv[0]);
 		write(2, err_msg, _strlen(err_msg));
-		free(str);
+		(i == 1) ? free(str) : i;
 		return (127);
 	}
 	return (0);
